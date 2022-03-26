974d1c355c790c23d3523eb6721a6ca963b278de
Rich Lobo <lobo.r0018@gmail.com>
2022-03-26 22:27:47 +0800

    Create a function to change the status of the button when clicked

--- show-hide-button/src/App.js
+++ show-hide-button/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
+  const [status, setStatus] = useState('open')
+
+  const changeButtonStatus = () => {
+    console.log('button clicked')
+    status == 'open' ? setStatus('close') : setStatus('open')
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {status == 'open' ? <Button text='Click to subscribe!' onClick={changeButtonStatus}/> : <p>Thank you for subscribing!</p> }
     </div>
   );
 }
