1b1e0c4e0df1388b917ea3d0f59bf34504241a32
Rich Lobo <lobo.r0018@gmail.com>
2022-03-27 21:32:55 +0800

    Add function to increment and decrement counter using useState hook

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  
+  const add = () => {
+    setCount(count+1)
+  }
+
+  const subtract = () => {
+    setCount(count-1)
+  }
+  
   return (
     <div className="App">
-      
+      <button onClick={add}>+</button>
+      <input type='number' value={count}/>
+      <button onClick={subtract}>-</button>
     </div>
   );
 }
